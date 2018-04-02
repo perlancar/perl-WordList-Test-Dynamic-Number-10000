036f3d724196246f2ba8c9d2fefdd11b5aea78b6
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-04-02 16:41:31 +0700

    Initial commit

---
+++ lib/WordList/Test/Dynamic/Number/10000.pm
@@ -0,0 +1,22 @@
+package WordList::Test::Dynamic::Number::10000;
+
+# DATE
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+sub each_word {
+    my ($self, $code) = @_;
+
+    for my $i (1..10_000) {
+        my $word = sprintf "%05d", $i;
+        my $res = $code->($word);
+        last if defined $res && $res == -2;
+    }
+}
+
+# STATS
+
+1;
+# ABSTRACT: 10 million numbers from "00001" to "10000"
